e819866a6869856c9fa029b79e6367f9cab98aa3
shikubro90 <shikubro90@gmail.com>
2022-02-09 01:42:45 +0600

    lesson-10 done

--- src/components/Account.js
+++ src/components/Account.js
@@ -5,7 +5,7 @@ const Account = ()=>{
         <span className="material-icons-outlined" title="Account">
           account_circle
         </span>
-        <a href="signup.html">Signup</a>
+        <a href="signup">Signup</a>
         {/* <span class="material-icons-outlined" title="Logout"> logout </span> */}
       </div>
     )
--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,27 @@
 import "../styles/App.css"
 import Layout from "./Layout";
-// import Login from "./pages/Login";
-// import Home from "./pages/Home";
-// import Signup from "./pages/Signup";
-import Nav from "./Nav";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
 import Quize from "./pages/Quize";
+import Result from "./pages/Result";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
 
 function App() {
   return (
-    <Layout>
-      
-      <Nav/>
 
-      {/* <Home/>
-      
-      <Signup/>
+    <Router>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/quize" component={Quize} />
+          <Route exact path="/result" component={Result} />
+        </Switch>
+      </Layout>
+    </Router>
 
-      <Login /> */}
-
-      <Quize/>
-      
-
-    </Layout>
   );
 }
 
