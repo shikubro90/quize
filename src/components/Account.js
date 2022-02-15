964d9c6d2ec8e71945dbf6938b59b5b71b253219
shikubro90 <shikubro90@gmail.com>
2022-02-16 02:33:17 +0600

    env git open

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,15 +1,29 @@
 import Classes from "../styles/Account.module.css"
 import {Link} from "react-router-dom"
+import { useAuth } from "../contex/AuthContext"
 const Account = ()=>{
-    return(
-        <div className={Classes.account}>
-        <span className="material-icons-outlined" title="Account">
-          account_circle
-        </span>
-        <Link to="/signup">Signup</Link>
-        <Link to="login">Login</Link>
-        {/* <span class="material-icons-outlined" title="Logout"> logout </span> */}
 
+  const {currentUser, logOut} = useAuth()
+
+    return(
+      <div className={Classes.account}>
+        {
+          currentUser ? (
+            <>
+          <span>{currentUser.displayName}</span>
+          <span className="material-icons-outlined" title="Account">
+            account_circle
+          </span>
+          <span className="material-icons-outlined" title="Logout" onClick={logOut}> logout </span>
+        </>
+          ) : (
+            <>
+              <Link to="/signup">Signup</Link>
+              <Link to="login">Login</Link>
+            </>
+          )
+        }
+        
       </div>
     )
 }
